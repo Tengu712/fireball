3756eaa428f971f466cb8f68938944706b90c102
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2022-07-06 02:54:31 +0900

    select physical device

--- src/graphics/vulkan.rs
+++ src/graphics/vulkan.rs
@@ -11,13 +11,19 @@ type PFN_vkInternalAllocationNotification = *const c_void; // !
 type PFN_vkInternalFreeNotification = *const c_void; // !
 #[allow(non_camel_case_types)]
 type PFN_vkReallocationFunction = *const c_void; // !
+type VkDeviceSize = u64;
 type VkFlags = u32;
 /// Incomplete type. `typedef struct VkInstance_T* VkInstance;`
 type VkInstance = *const c_void;
 type VkInstanceCreateFlags = VkFlags;
+type VkMemoryHeapFlags = VkFlags;
+type VkMemoryPropertyFlags = VkFlags;
+/// Incomplete type. `typedef struct VkPhysicalDevice_T* VkPhysicalDevice;`
+type VkPhysicalDevice = *const c_void;
 type VkResult = i32;
 type VkStructureType = u32;
 
+const VK_MAX_MEMORY_HEAPS: usize = 16;
 const VK_STRUCTURE_TYPE_APPLICATION_INFO: VkStructureType = 0;
 const VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO: VkStructureType = 1;
 const VK_SUCCESS: VkResult = 0;
@@ -58,6 +64,18 @@ struct VkApplicationInfo {
 }
 #[repr(C)]
 #[allow(non_snake_case)]
+struct VkMemoryHeap {
+    size: VkDeviceSize,
+    flags: VkMemoryHeapFlags,
+}
+#[repr(C)]
+#[allow(non_snake_case)]
+struct VkMemoryType {
+    propertyFlags: VkMemoryPropertyFlags,
+    heapIndex: u32,
+}
+#[repr(C)]
+#[allow(non_snake_case)]
 struct VkInstanceCreateInfo {
     sType: VkStructureType,
     pNext: *const c_void,
@@ -68,6 +86,14 @@ struct VkInstanceCreateInfo {
     enabledExtensionCount: u32,
     ppEnabledExtensionNames: *const *const c_char,
 }
+#[repr(C)]
+#[allow(non_snake_case)]
+struct VkPhysicalDeviceMemoryProperties {
+    memoryTypeCount: u32,
+    memoryTypes: [VkMemoryType; VK_MAX_MEMORY_HEAPS],
+    memoryHeapCount: u32,
+    memoryHeaps: [VkMemoryHeap; VK_MAX_MEMORY_HEAPS],
+}
 
 #[link(name = "vulkan-1")]
 extern "C" {
@@ -76,36 +102,76 @@ extern "C" {
         pAllocator: *const VkAllocationCallbacks,
         pInstance: *mut VkInstance,
     ) -> VkResult;
+    fn vkEnumeratePhysicalDevices(
+        instance: VkInstance,
+        pPhysicalDeviceCount: *mut u32,
+        pPhysicalDevices: *mut VkPhysicalDevice,
+    ) -> VkResult;
+    fn vkGetPhysicalDeviceMemoryProperties(
+        physicalDevice: VkPhysicalDevice,
+        pMemoryProperties: *mut VkPhysicalDeviceMemoryProperties,
+    ) -> c_void;
 }
 
 pub struct Vulkan;
 impl Vulkan {
     pub fn new(appname: &'static str) -> Self {
-        let appname_cstr = CString::new(appname).unwrap();
-        let app_info = VkApplicationInfo {
-            sType: VK_STRUCTURE_TYPE_APPLICATION_INFO,
-            pNext: std::ptr::null(),
-            pApplicationName: appname_cstr.as_ptr(),
-            applicationVersion: 0,
-            pEngineName: appname_cstr.as_ptr(),
-            engineVersion: VK_MAKE_VERSION(1, 0, 0),
-            apiVersion: VK_API_VERSION_1_1(),
-        };
-        let create_info = VkInstanceCreateInfo {
-            sType: VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-            pNext: std::ptr::null(),
-            flags: 0,
-            pApplicationInfo: &app_info,
-            enabledLayerCount: 0,
-            ppEnabledLayerNames: std::ptr::null(),
-            enabledExtensionCount: 0,
-            ppEnabledExtensionNames: std::ptr::null(),
-        };
-        let mut instance = std::ptr::null();
-        let res = unsafe { vkCreateInstance(&create_info, std::ptr::null(), &mut instance) };
-        if res != VK_SUCCESS {
-            panic!("[fatal error] failed to create vulkan instance. : {}", res);
-        }
+        let instance = create_instance(appname);
+        let (_, _) = select_physical_device(&instance);
         Self
     }
+}
+fn create_instance(appname: &'static str) -> VkInstance {
+    let appname_cstr = CString::new(appname).unwrap();
+    let app_info = VkApplicationInfo {
+        sType: VK_STRUCTURE_TYPE_APPLICATION_INFO,
+        pNext: std::ptr::null(),
+        pApplicationName: appname_cstr.as_ptr(),
+        applicationVersion: 0,
+        pEngineName: appname_cstr.as_ptr(),
+        engineVersion: VK_MAKE_VERSION(1, 0, 0),
+        apiVersion: VK_API_VERSION_1_1(),
+    };
+    let create_info = VkInstanceCreateInfo {
+        sType: VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+        pNext: std::ptr::null(),
+        flags: 0,
+        pApplicationInfo: &app_info,
+        enabledLayerCount: 0,
+        ppEnabledLayerNames: std::ptr::null(),
+        enabledExtensionCount: 0,
+        ppEnabledExtensionNames: std::ptr::null(),
+    };
+    let mut instance = std::ptr::null();
+    let res = unsafe { vkCreateInstance(&create_info, std::ptr::null(), &mut instance) };
+    assert!(
+        res == VK_SUCCESS,
+        "[fatal error] failed to create vulkan instance. : {}",
+        res
+    );
+    instance
+}
+fn select_physical_device(
+    instance: &VkInstance,
+) -> (VkPhysicalDevice, VkPhysicalDeviceMemoryProperties) {
+    let mut cnt = 0;
+    let res = unsafe { vkEnumeratePhysicalDevices(*instance, &mut cnt, std::ptr::null_mut()) };
+    assert!(
+        res == VK_SUCCESS,
+        "[fatal error] failed to get the number of physical devices. : {}",
+        res
+    );
+    let devices = unsafe {
+        std::alloc::alloc(std::alloc::Layout::new::<VkPhysicalDevice>()) as *mut VkPhysicalDevice
+    };
+    let res = unsafe { vkEnumeratePhysicalDevices(*instance, &mut cnt, devices) };
+    assert!(
+        res == VK_SUCCESS,
+        "[fatal error] failed to enumerate physical devices. : {}",
+        res
+    );
+    let device = unsafe { *devices };
+    let mut props = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+    unsafe { vkGetPhysicalDeviceMemoryProperties(device, &mut props) };
+    (device, props)
 }
